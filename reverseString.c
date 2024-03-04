7de951e446374462627a5cbda30c0d7949330c1c
Duncan <dshitubi@outlook.com>
2024-03-04 15:10:59 -0500

    Fixed reverse-string function not to modify the string

---
+++ reverseString.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define LIM 1000
+#define IN 1
+#define OUT 0
+
+void reverseString(char s [LIM], int len);
+
+/*reverse string using while loop swapping towards the middle*/
+
+int main (){
+    /*get string from while loop*/
+    int c, len=0;
+    char s[LIM]={""};
+    int state = OUT;
+
+    while ((c = getchar()) != EOF ){
+        if(c != '\n' && c != ' ' && c != '\t' && c != EOF){
+            state = IN;
+            s[len] = c;
+            ++len;
+        } else if(state == IN){
+            state = OUT;
+            reverseString(s, len-1);
+            s[len+1] = '\0';
+            printf("%s \n", s);
+            len = 0;
+            memset(s,'\0',LIM);
+        }
+    }
+    printf( "\n");
+}
+
+void reverseString(char s[LIM], int end){
+    char temp;
+    int start = 0;
+
+    while( start <
+     end){
+        temp=s[start];
+        s[start] = s[end];
+        s[end] = temp;
+        ++start;
+        --  end;
+    }
+}
